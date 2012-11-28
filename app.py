c7ae03c9876684fdb7b4d2a6258ed6870ebac642
Christopher Groskopf <staringmonkey@gmail.com>
2012-11-28 14:42:58 -0600

    Everything but bootstrap works.

--- app.py
+++ app.py
@@ -1,37 +1,29 @@
 #!/usr/bin/env python
 
 import csv
-from mimetypes import guess_type
 
+import envoy
 from flask import Flask, render_template
 
 import app_config
-from render_utils import JavascriptIncluder
-
+from render_utils import make_context 
 app = Flask(app_config.PROJECT_NAME)
 
-# Utils
-def make_context():
-    context = app_config.__dict__
-    context['JS'] = JavascriptIncluder(debug=app.debug)
-
-    return context
-
-# App-specific views 
+# Example application views 
 @app.route('/')
 @app.route('/simple')
 def simple():
     """
     Example view demonstrating rendering a simple HTML page.
     """
-    return render_template('simple.html', **make_context())
+    return render_template('simple.html', **make_context(app))
 
 @app.route('/table')
 def table():
     """
     Example view demonstrating rendering a table page.
     """
-    context = make_context() 
+    context = make_context(app) 
 
     with open('data/example.csv') as f:
         reader = csv.reader(f)
@@ -45,13 +37,30 @@ def map():
     """
     TODO: Example view demonstrating rendering a map page.
     """
-    return render_template('map.html', **make_context())
+    return render_template('map.html', **make_context(app))
 
-foo="""# Generic routing for static files
+# Render JST templates on-demand
+@app.route('/js/templates.js')
+def _templates_js():
+    r = envoy.run('node_modules/.bin/jst --template underscore jst')
+
+    return r.std_out, 200, { 'Content-Type': 'application/javascript' }
+
+# Render arbitrary javascript files on-demand
 @app.route('/js/<path:path>')
 def _js(path):
-    with open('www/%s' % path) as f:
-        return f.read(), 200, { 'Content-Type': guess_type(path)[0] }"""
+    with open('js/%s' % path) as f:
+        return f.read(), 200, { 'Content-Type': 'application/javascript' }
+
+# Render LESS files on-demand
+@app.route('/less/<string:filename>')
+def _less(filename):
+    with open('less/%s' % filename) as f:
+        less = f.read()
+
+    r = envoy.run('node_modules/.bin/lessc -', data=less)
+
+    return r.std_out, 200, { 'Content-Type': 'text/css' }
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
--- render_utils.py
+++ render_utils.py
@@ -2,6 +2,8 @@
 
 from flask import Markup
 
+import app_config
+
 class JavascriptIncluder():
     """
     Psuedo-template tag that handles collecting Javascript and serving appropriate clean or compressed versions.
@@ -29,4 +31,39 @@ class JavascriptIncluder():
         del self.includes[:]
 
         return Markup('<script type="text/javascript" src="%s"></script>' % path)
+
+class CSSIncluder():
+    """
+    Psuedo-template tag that handles collecting CSS and serving appropriate clean or compressed versions.
+    """
+    def __init__(self, debug):
+        self.debug = debug
+        self.includes = []
+
+    def push(self, path):
+        self.includes.append(path)
+
+        return ""
+
+    def render(self, path):
+        if self.debug:
+            response = ','.join(self.includes)
+            
+            response = '\n'.join([
+                '<link rel="stylesheet" type="text/css" href="%s" />' % css for css in self.includes
+                ])
+            del self.includes[:]
+
+            return Markup(response)
+        
+        del self.includes[:]
+
+        return Markup('<link rel="stylesheet" type="text/css" href="%s" />' % path)
+
+def make_context(app):
+    context = app_config.__dict__
+    context['JS'] = JavascriptIncluder(debug=app.debug)
+    context['CSS'] = CSSIncluder(debug=app.debug)
+
+    return context
 
