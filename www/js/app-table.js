dbf9d041814672328a8f186769104e372ce5a9ce
Christopher Groskopf <staringmonkey@gmail.com>
2012-12-13 14:30:20 -0600

    Refactor example table js into its own file.

---
+++ www/js/app-table.js
@@ -0,0 +1,20 @@
+// Setup tables
+var $table = $(".app-table");
+
+$(function() {
+    // Tables example
+    var table = $table.find(".table").tablesorter({
+        widthFixed: true,
+        widgets: ['zebra']
+    });
+
+    table.tablesorterPager({
+        container: $table.find(".table-pager"),
+        positionFixed: false,
+        size: 10
+    });
+
+    table.tablesorterMultiPageFilter({
+        filterSelector: $table.find(".table-filter input")
+    });
+});
--- www/js/app.js
+++ www/js/app.js
@@ -1,25 +1,6 @@
-// Setup tables
-var $table = $(".app-table");
-
 $(function() {
     // JST example
     html = JST.example({ "name": "foobar" });
 
     console.log(html);
-
-    // Tables example
-    var table = $table.find(".table").tablesorter({
-        widthFixed: true,
-        widgets: ['zebra']
-    });
-
-    table.tablesorterPager({
-        container: $table.find(".table-pager"),
-        positionFixed: false,
-        size: 10
-    });
-
-    table.tablesorterMultiPageFilter({
-        filterSelector: $table.find(".table-filter input")
-    });
 });
