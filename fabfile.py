c765417a93b1eea5992a584d8e187cfd11332918
Christopher Groskopf <staringmonkey@gmail.com>
2013-03-11 15:10:51 -0500

    Never deploy live-data to S3. Closes #54.

--- fabfile.py
+++ fabfile.py
@@ -243,8 +243,8 @@ def _deploy_to_s3():
     """
     Deploy the gzipped stuff to
     """
-    s3cmd = 's3cmd -P --add-header=Cache-Control:max-age=5 --guess-mime-type --recursive --exclude-from gzip_types.txt sync gzip/ %s'
-    s3cmd_gzip = 's3cmd -P --add-header=Cache-Control:max-age=5 --add-header=Content-encoding:gzip --guess-mime-type --recursive --exclude "*" --include-from gzip_types.txt sync gzip/ %s'
+    s3cmd = 's3cmd -P --add-header=Cache-Control:max-age=5 --guess-mime-type --recursive --exclude live-data/* --exclude-from gzip_types.txt sync gzip/ %s'
+    s3cmd_gzip = 's3cmd -P --add-header=Cache-Control:max-age=5 --add-header=Content-encoding:gzip --guess-mime-type --recursive --exclude live-data/* --exclude "*" --include-from gzip_types.txt sync gzip/ %s'
 
     for bucket in env.s3_buckets:
         env.s3_bucket = bucket
