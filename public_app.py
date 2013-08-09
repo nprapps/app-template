2fefc26d022a6b9d83684aecfcd256fe0ccbbc56
Christopher Groskopf <staringmonkey@gmail.com>
2013-08-09 08:21:24 -0500

    Add test route to public_app.py. #194.

--- public_app.py
+++ public_app.py
@@ -2,21 +2,18 @@
 
 import datetime
 
-from flask import Flask, render_template
+from flask import Flask
 
 import app_config
 
 app = Flask(app_config.PROJECT_NAME)
 
-
-# Example application views
-@app.route('/%s/' % app_config.PROJECT_SLUG)
-def _dynamic_page():
+@app.route('/%s/test/' % app_config.PROJECT_SLUG, methods=['GET'])
+def _test_app():
     """
-    Example dynamic view demonstrating rendering a simple HTML page.
+    Test route for verifying the application is running.
     """
-    return datetime.datetime.now().isoformat()
-
+    return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=app_config.DEBUG)
