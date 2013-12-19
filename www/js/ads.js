6afabb167353a29a6f80dca29399118c5ee628a4
Christopher Groskopf <staringmonkey@gmail.com>
2013-12-19 16:58:53 -0600

    responsive-ad.js is dead. Long live ads.js

--- www/js/ads.js
+++ www/js/ads.js
@@ -1,9 +1,68 @@
+var ADS = {};
+var googletag = { cmd: [] };
+
+/*
+ * Are we on a tablet?
+ */
+ADS.isOnTablet = function() {
+    var width = window.innerWidth;
+
+    return (width >= 768 && width <= 1024);
+}
+
+/*
+ * Are we on a phone?
+ */
+ADS.isOnPhone = function() {
+    var width = window.innerWidth;
+
+    return width <= 767;
+}
+
+/*
+ * Logic to determine if we should render a certain ad.
+ */
+ADS.shouldRenderForDevice = function(deviceEnv) {
+    if (!deviceEnv) {
+        return false;
+    } else {
+        var shouldRender = false;
+        var winWidth = $(window).width();
+        var winOrientation = window.orientation;
+
+        switch (deviceEnv) {
+            case 'desktop':
+                var ieEightCheck = ($.browser.msie === true && ($.browser.version === '7.0' || $.browser.version === '8.0'));
+                if (ieEightCheck) {
+                    shouldRender = true;
+                } else if (!ADS.isOnTablet() && winWidth > 1024) {
+                    shouldRender = true;
+                }
+                
+                break;
+            case 'mobile':
+                if ((ADS.isOnPhone() || ADS.isOnTablet()) && winWidth >= 300 && winWidth <= 1024) {
+                    // block ads from ever showing on small-screen mobile devices
+                    if (winWidth >= 480 || winOrientation == 0 || winOrientation == 180) {
+                        shouldRender = true;
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+		//console.log(deviceEnv + ' shouldRender: ' + shouldRender);
+        return shouldRender;
+    }
+}
+
+
 /*
  * Configure our ad slots and targeting.
  */
-function setup_ads() {
+ADS.setup_ads = function() {
     // Desktop ad slot
-    if (DFP.shouldRenderForDevice('desktop')) {
+    if (ADS.shouldRenderForDevice('desktop')) {
         googletag.defineSlot('/6735/n6735.' + APP_CONFIG.NPR_DFP.ENVIRONMENT + '/' + APP_CONFIG.NPR_DFP.TARGET, [[300,250]], 'ad-desktop').addService(googletag.pubads());
     }
 
@@ -20,28 +79,10 @@ function setup_ads() {
         googletag.pubads().setTargeting('device', 'ios');
     }
 
-    // ????
-    if (typeof DFP.queryParameters.sc === 'string' && DFP.queryParameters.sc.length > 0) {
-        scValue = DFP.queryParameters.sc;
-    } else if (typeof document.referrer === 'string' && document.referrer.search('facebook.com') > -1) {
-        scValue = 'fb';
-    } else if (typeof DFP.queryParameters.ft === 'string') {
-        scValue = DFP.queryParameters.ft;
-    }
-    
-    googletag.pubads().setTargeting('sc', scValue);
-
-    // Timezones
-    var utm_source = typeof DFP.queryParameters.utm_source === 'string' && DFP.queryParameters.utm_source.length > 0?DFP.queryParameters.utm_source:'';
-    var utm_medium = typeof DFP.queryParameters.utm_medium === 'string' && DFP.queryParameters.utm_medium.length > 0?DFP.queryParameters.utm_medium:'';
-
-    googletag.pubads().setTargeting('utm_source', utm_source);
-    googletag.pubads().setTargeting('utm_medium', utm_medium);
-
     // Orientation
     var orient = '';
 
-    if (NPR.Devices.isOnPhone() || NPR.Devices.isOnTablet()) {
+    if (ADS.isOnPhone() || ADS.isOnTablet()) {
         if (window.orientation == 0 || window.orientation == 180) {
             orient = 'portrait';
         } else if (window.orientation == 90 || window.orientation == -90) {
@@ -56,14 +97,28 @@ function setup_ads() {
     googletag.enableServices();
 }
 
+/*
+ * Load Google JS.
+ */
+ADS.setup_googletag = function() {
+    var gads = document.createElement('script');
+    gads.async = true;
+    gads.type = 'text/javascript';
+    var useSSL = 'https:' == document.location.protocol;
+    gads.src = (useSSL ? 'https:' : 'http:') + '//www.googletagservices.com/tag/js/gpt.js';
+    var node = document.getElementsByTagName('script')[0];
+    node.parentNode.insertBefore(gads, node);
+};
+
 /* 
  * Execute configuration when Google is ready.
  */
 googletag.cmd.push(function() {
     try {
-        setup_ads();
+        ADS.setup_ads();
     } catch(e) {
         NPR.messaging.exception(e, 'Define Google Publisher Tags', NPR.messaging.constants.SPONSORSHIP_ERROR);
     }
 });
 
+ADS.setup_googletag();
