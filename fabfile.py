15609b05e9e934ee8a1c593ca879bc077f71cc58
Christopher Groskopf <staringmonkey@gmail.com>
2014-02-05 13:46:16 -0600

    Backport new assets rig from musicgame. Closes #299.

--- fabfile.py
+++ fabfile.py
@@ -9,6 +9,7 @@ from jinja2 import Template
 
 import app
 import app_config
+import boto
 from etc import github
 from etc.gdocs import GoogleDoc
 
@@ -162,7 +163,7 @@ def render():
     from flask import g
 
     update_copy()
-    sync_assets()
+    assets_sync()
     update_data()
     less()
     jst()
@@ -330,7 +331,7 @@ def bootstrap():
 
     local('npm install less universal-jst -g --prefix node_modules')
 
-    assets_down()
+    assets_sync()
     update_copy()
     update_data()
 
@@ -367,46 +368,6 @@ def _deploy_to_s3(path='.gzip'):
         local(sync % (path, 's3://%s/%s/' % (bucket, app_config.PROJECT_SLUG)))
         local(sync_gzip % (path, 's3://%s/%s/' % (bucket, app_config.PROJECT_SLUG)))
         local(sync_assets % ('www/assets/', 's3://%s/%s/assets/' % (bucket, app_config.PROJECT_SLUG)))
-
-def assets_down(path='www/assets'):
-    """
-    Download assets folder from s3 to www/assets
-    """
-    local('aws s3 sync s3://%s/%s/ %s/ --acl "public-read" --cache-control "max-age=5" --region "us-east-1"' % (app_config.ASSETS_S3_BUCKET, app_config.PROJECT_SLUG, path))
-
-def assets_up(path='www/assets'):
-    """
-    Upload www/assets folder to s3
-    """
-    _confirm("You are about to replace the copy of the folder on the server with your own copy. Are you sure?")
-
-    local('aws s3 sync %s/ s3://%s/%s/ --acl "public-read" --cache-control "max-age=5" --region "us-east-1" --delete' % (
-            path,
-            app_config.ASSETS_S3_BUCKET,
-            app_config.PROJECT_SLUG
-        ))
-
-def assets_rm(path):
-    """
-    remove an asset from s3 and locally
-    """
-    file_list = glob(path)
-
-    if len(file_list) > 0:
-
-        _confirm("You are about to destroy %s files. Are you sure?" % len(file_list))
-
-        with settings(warn_only=True):
-
-            for file_path in file_list:
-
-                local('aws s3 rm s3://%s/%s/%s --region "us-east-1"' % (
-                    app_config.ASSETS_S3_BUCKET,
-                    app_config.PROJECT_SLUG,
-                    file_path.replace('www/assets/', '')
-                ))
-
-                local('rm -rf %s' % path)
 
 def _gzip(in_path='www', out_path='.gzip'):
     """
@@ -517,6 +478,10 @@ def deploy(remote='origin'):
         _confirm("You are trying to deploy the '%s' branch to production.\nYou should really only deploy a stable branch.\nDo you know what you're doing?" % env.branch)
 
     if app_config.DEPLOY_TO_SERVERS:
+        fabcast('update_copy')
+        fabcast('assets_sync')
+        fabcast('update_data')
+
         checkout_latest(remote)
 
         if app_config.DEPLOY_CRONTAB:
@@ -540,6 +505,185 @@ def cron_test():
     require('settings', provided_by=[production, staging])
 
     local('echo $DEPLOYMENT_TARGET > /tmp/cron_test.txt')
+
+"""
+Assets
+"""
+def assets_sync():
+    """
+    Intelligently synchronize assets between S3 and local folder.
+    """
+    local_paths = []
+
+    for local_path, subdirs, filenames in os.walk('www/assets'):
+        for name in filenames:
+            local_paths.append(os.path.join(local_path, name))
+
+    bucket = _assets_get_bucket()
+    keys = bucket.list(app_config.PROJECT_SLUG)
+
+    which = None
+    always = False
+
+    for key in keys:
+        download = False
+        upload = False
+
+        local_path = key.name.replace(app_config.PROJECT_SLUG, 'www/assets', 1)
+
+        print local_path
+
+        if local_path in local_paths:
+            # A file can only exist once, this speeds up future checks
+            # and provides a list of non-existing files when complete
+            local_paths.remove(local_path)
+
+            # We need an actual key, not a "list key"
+            # http://stackoverflow.com/a/18981298/24608
+            key = bucket.get_key(key.name)
+
+            with open(local_path, 'rb') as f:
+                local_md5 = key.compute_md5(f)[0]
+
+            # Hashes are different
+            if key.get_metadata('md5') != local_md5:
+                if not always:
+                    # Ask user which file to take
+                    which, always = _assets_confirm(local_path)
+
+                if not which:
+                    print 'Cancelling!'
+
+                    return
+
+                if which == 'remote':
+                    download = True
+                elif which == 'local':
+                    upload = True
+        else:
+            download = True
+            
+        if download:
+            _assets_download(key, local_path)
+
+        if upload:
+            _assets_upload(local_path, key)
+
+    action = None
+    always = False
+
+    # Iterate over files that didn't exist on S3
+    for local_path in local_paths:
+        key_name = local_path.replace('www/assets', app_config.PROJECT_SLUG, 1)
+        key = bucket.get_key(key_name, validate=False)
+
+        print local_path
+
+        if not always:
+            action, always = _assets_upload_confirm()
+
+        if not action:
+            print 'Cancelling!'
+
+            return
+
+        if action == 'upload':
+            _assets_upload(local_path, key)
+        elif action == 'delete':
+            _assets_delete(local_path, key)
+
+def _assets_get_bucket():
+    """
+    Get a reference to the assets bucket.
+    """
+    s3 = boto.connect_s3()
+    
+    return s3.get_bucket(app_config.ASSETS_S3_BUCKET)
+
+def _assets_confirm(local_path):
+    """
+    Check with user about whether to keep local or remote file.
+    """
+    print '--> This file has been changed locally and on S3.'
+    answer = prompt('Take remote [r] Take local [l] Take all remote [ra] Take all local [la] cancel', default='c')
+
+    if answer == 'r':
+        return ('remote', False)
+    elif answer == 'l':
+        return ('local', False)
+    elif answer == 'ra':
+        return ('remote', True)
+    elif answer == 'la':
+        return ('local', True)
+        
+    return (None, False)
+
+def _assets_upload_confirm():
+    print '--> This file does not exist on S3.'
+    answer = prompt('Upload local copy [u] Delete local copy [d] Upload all [ua] Delete all [da] cancel', default='c')
+
+    if answer == 'u':
+        return ('upload', False)
+    elif answer == 'd':
+        return ('delete', False)
+    elif answer == 'ua':
+        return ('upload', True)
+    elif answer == 'da':
+        return ('download', True)
+
+    return (None, False) 
+
+def _assets_download(s3_key, local_path):
+    """
+    Utility method to download a single asset from S3.
+    """
+    print '--> Downloading!' 
+
+    dirname = os.path.dirname(local_path)
+
+    if not (os.path.exists(dirname)):
+        os.makedirs(dirname)
+    
+    s3_key.get_contents_to_filename(local_path)
+
+def _assets_upload(local_path, s3_key):
+    """
+    Utility method to upload a single asset to S3.
+    """
+    print '--> Uploading!'
+    
+    with open(local_path, 'rb') as f:
+        local_md5 = s3_key.compute_md5(f)[0]
+
+    s3_key.set_metadata('md5', local_md5)
+    s3_key.set_contents_from_filename(local_path)
+
+def _assets_delete(local_path, s3_key):
+    """
+    Utility method to delete assets both locally and remotely.
+    """
+    print '--> Deleting!'
+
+    s3_key.delete()
+    os.remove(local_path)
+
+def assets_rm(path):
+    """
+    Remove an asset from s3 and locally
+    """
+    bucket = _assets_get_bucket()
+
+    file_list = glob(path)
+
+    if len(file_list) > 0:
+        _confirm("You are about to destroy %i files. Are you sure?" % len(file_list))
+
+        for local_path in file_list:
+            key_name = local_path.replace('www/assets', app_config.PROJECT_SLUG, 1)
+            key = bucket.get_key(key_name)
+            
+            _assets_delete(local_path, key)
+
 
 """
 Destruction
