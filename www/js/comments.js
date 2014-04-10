b8f7eb1e6b68dc63c4b734b7ea4871c88709a94a
Christopher Groskopf <staringmonkey@gmail.com>
2014-04-10 13:30:33 -0500

    Very rough comments on index.html.

--- www/js/comments.js
+++ www/js/comments.js
@@ -1,6 +1,33 @@
-var renderComments = function() {
+var $comments = null;
+var $showComments = null;
+var $hideComments = null;
+var fullpage = false;
+
+var onShowCommentsClick = function() {
+    $comments.show();
+
+    return false;
+}
+
+var onHideCommentsClick = function() {
+    $comments.hide();
+
+    return false;
+}
+
+$(function() {
+    $comments = $('#comments');
+    $showComments = $('.show-comments');
+    $hideComments = $('.hide-comments');
+    fullpage = $comments.hasClass('fullpage');
+
     var context = $.extend(APP_CONFIG, {});
     var html = JST.comments();
 
-    $('#comments').html(html);
-}
+    $comments.html(html);
+
+    console.log($showComments);
+
+    $showComments.on('click', onShowCommentsClick);
+    $hideComments.on('click', onHideCommentsClick);
+});
