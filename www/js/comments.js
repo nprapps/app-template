f38d762f9f5d4e34cb1cc93c7306fc6500333834
Jeremy Bowers <jeremyjbowers@gmail.com>
2014-04-18 10:54:24 -0400

    Adds some documentation. Adds a watcher for the comment pane.

--- www/js/comments.js
+++ www/js/comments.js
@@ -1,6 +1,4 @@
 var EVENT_CATEGORY = APP_CONFIG.PROJECT_SLUG;
-
-
 var $comments = null;
 var $commentButton = null;
 var $commentCount = null;
@@ -16,10 +14,17 @@ var getAuthCookie = function() {
     * Grabs the auth cookie.
     * Returns the results trimmed and URL-decoded.
     */
+
+    // Get all cookies this domain can see.
     var allCookies = document.cookie.split(';');
 
+    // Get our cookie, 'at,' and return it trimmed and deurlified.
     for (var i = 0; i < allCookies.length; i++) {
+
+        // Our cookie is called 'at'. For some reason.
         if ($.trim(allCookies[i].split('=')[0]) == 'at') {
+
+            // Send it out!
             return decodeURIComponent($.trim(allCookies[i].split('=')[1]));
         }
     }
@@ -28,24 +33,35 @@ var getAuthCookie = function() {
 }
 
 var parseAuthCookie = function() {
+    /*
+    * Parses the cookie out to an object.
+    * Returns an object with the necessary auth components to renew a cookie.
+    */
     var cookie = getAuthCookie();
 
+    // If there's no cookie, return an empty object.
     if (!cookie) {
         return {};
     }
 
+    // Our cookie data is a urlencoded string. So split on &'s.
     var data = cookie.split('&');
     var parsed = {};
 
+    // Loop over the key/value pairs.
     for (pair in data) {
+
+        // Split to keys and values.
         var bits = data[pair].split('=');
         var key = bits[0];
         var val = bits[1];
 
+        // This is a great story. You should ask @onyxfish about it.
         if (key != 'email') {
             val = val.replace(/\+/g, " ");
         }
 
+        // Decode the value, just in case.
         parsed[key] = decodeURIComponent(val);
     }
 
@@ -53,11 +69,20 @@ var parseAuthCookie = function() {
 }
 
 var loadDisqusFrame = function() {
+    /*
+    * Writes the Disqus frame and sets some configuration.
+    */
+
+    // Get the auth bits from the cookie.
     var auth = parseAuthCookie();
 
+    // If we've got our auth bits, set up a callback function.
     if (typeof auth['dt'] !== 'undefined') {
+
+        // Hide the login bits.
         $('#login-overlay-link').hide();
 
+        // Set up a callback function to use later. Apparently.
         disqus_config = function () {
             this.page.remote_auth_s3 = auth['dm'] + ' ' + auth['dh'] + ' ' + auth['dt'];
             this.page.api_key = 'K5ANvxVxS7meX7au7vJpUpqIgFqQcDBEH8q39Z8N750SFmBhaOLTsShueMWid956';
@@ -69,10 +94,15 @@ var loadDisqusFrame = function() {
     * They suggest that we do not modify the code below here.
     * Seriously. Hands (and feet) off.
     */
-    var dsq=document.createElement('script');dsq.type='text/javascript';dsq.async=true;dsq.src='//'+disqus_shortname+'.disqus.com/embed.js';(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(dsq);
+    var dsq=document.createElement('script');dsq.type='text/javascript';dsq.async=true;
+    dsq.src='//'+disqus_shortname+'.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(dsq);
 }
 
 var onCommentButtonClick = function() {
+    /*
+    * Click handler for the show comments button.
+    */
     if ( $comments.hasClass('show') ) {
         $comments.removeClass('show');
     } else {
@@ -80,7 +110,23 @@ var onCommentButtonClick = function() {
         _gaq.push(['_trackEvent', EVENT_CATEGORY, 'Clicked to reveal comments']);
     }
 
+    // Doesn't load the Disqus frame (or check auth) until this is clicked.
     loadDisqusFrame();
+
+    // Check to see if the comments pane has been open for at least 10 seconds.
+    var readingCommentsTimeout;
+
+    var onReadingComments = function() {
+
+        // Push an event to google analytics.
+        _gaq.push(['_trackEvent', EVENT_CATEGORY, 'Read comments for 10 seconds']);
+
+        // Clear the timeout.
+        window.clearTimeout(readingCommentsTimeout);
+    }
+
+    // Ten. Seconds.
+    readingCommentsTimeout = window.setTimeout(onReadingComments, 10000);
 
     return false;
 }
@@ -116,25 +162,32 @@ var loadCommentCount = function() {
 }
 
 $(function() {
+
+    // Bind the DOM elements we care about.
     $comments = $('#comments');
     $commentButton = $('.comment-button');
     $commentCount = $('.comment-count');
 
+    // Set some global state.
     fullpage = $comments.hasClass('fullpage');
     disqus_shortname = APP_CONFIG.DISQUS_SHORTNAME;
     disqus_identifier = disqus_shortname + '-comments';
 
+    // Set some vars for JST rendering, e.g., the comments frame.
     var context = $.extend(APP_CONFIG, {});
     var html = JST.comments();
-
     $comments.html(html);
 
+    // Click handler for the show comments button.
     $commentButton.on('click', onCommentButtonClick);
 
+    // If this is the pane view, show a little close-this-pane button.
     if (!fullpage) {
         $comments.find('.comments-close').on('click', onCommentButtonClick);
     }
 
+    // Get the comment count from the Disqus API.
     loadCommentCount();
+
 });
 
