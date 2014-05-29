82119e6a271a0982f8cca9303e0653b3a80a0af1
Christopher Groskopf <staringmonkey@gmail.com>
2014-05-29 10:16:38 -0700

    Factor analytics for easier reuse.

--- fabfile/analytics.py
+++ fabfile/analytics.py
@@ -4,22 +4,50 @@
 Analytics reporting tasks
 """
 
+from collections import OrderedDict
+import json
+import os
+
 from fabric.api import task
 
 import app_config
 from etc.ga import GoogleAnalytics
 
 @task
-def report():
+def machine(path):
     """
-    Query some things.
+    Write current analytics to a JSON file.
     """
     ga = GoogleAnalytics(slug=app_config.PROJECT_SLUG)
-    #ga = GoogleAnalytics(slug='commencement')
+
+    output = {}
+    print 'Getting totals'
+    output['totals'] = ga.totals()
+    print 'Getting top devices'
+    output['top_devices'] = ga.totals_by_device_category(output['totals'])
+    print 'Getting top sources'
+    output['top_sources'] = ga.totals_by_source(output['totals'])
+    print 'Getting top browsers'
+    output['top_browsers'] = ga.totals_by_browser(output['totals'])
+    print 'Getting top pages'
+    output['top_pageviews'] = ga.top_pageviews()
+    print 'Getting performance data'
+    output['performance'] = ga.performance()
+
+    with open(path, 'w') as f:
+        json.dump(output, f)
+
+@task
+def human(path):
+    """
+    Print analytics from a saved file.
+    """
+    with open(path) as f:
+        data = json.load(f, object_pairs_hook=OrderedDict)
 
     print 'Totals:'
 
-    totals = ga.totals()
+    totals = data['totals'] 
 
     for k, v in totals.items():
         print '{:>15,d}    {:s}'.format(v, k)
@@ -27,7 +55,7 @@ def report():
     print ''
     print 'Top devices:'
 
-    for column, devices in ga.totals_by_device_category(totals).items():
+    for column, devices in data['top_devices'].items():
         print '    %s' % column
 
         for d, v in devices.items():
@@ -36,7 +64,7 @@ def report():
     print ''
     print 'Top sources (pageviews):'
 
-    sources = ga.totals_by_source(totals)['ga:pageviews']
+    sources = data['top_sources']['ga:pageviews']
 
     for d, v in sources.items():
         print '{:>15.1%}    {:s}'.format(v, d)
@@ -44,7 +72,7 @@ def report():
     print ''
     print 'Top browsers (pageviews):'
 
-    browsers = ga.totals_by_browser(totals)['ga:pageviews']
+    browsers = data['top_browsers']['ga:pageviews']
 
     for d, v in browsers.items():
         print '{:>15.1%}    {:s}'.format(v, d)
@@ -52,14 +80,25 @@ def report():
     print ''
     print 'Top pageviews:'
 
-    for page, pageviews in ga.top_pageviews().items():
+    for page, pageviews in data['top_pageviews'].items():
         print '{:>15,d}    {:s}'.format(pageviews, page)
 
     print ''
     print 'Performance (seconds):'
 
-    metrics = ga.performance()
+    metrics = data['performance'] 
 
     for k, v in metrics.items():
         print '{:>15.1f}    {:s}'.format(v, k)
+
+
+@task(default=True)
+def report():
+    """
+    Print current analytics to the console.
+    """
+    machine('.temp_report.json')
+    human('.temp_report.json')
+    os.remove('.temp_report.json')
+
 
