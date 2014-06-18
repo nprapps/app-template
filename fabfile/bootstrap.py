26c23eb9288c2e799b15f45fd3bcdb09bb3b4361
Christopher Groskopf <staringmonkey@gmail.com>
2014-06-18 10:21:02 -0500

    Force add .assetsignore after bootstrap. Closes #386.

--- fabfile/bootstrap.py
+++ fabfile/bootstrap.py
@@ -37,6 +37,7 @@ def go(github_username='nprapps', repository_name=None):
     local('rm *.pyc')
     local('rm LICENSE')
     local('git add .')
+    local('git add -f www/assets/.assetsignore')
     local('git commit -am "Initial import from app-template."')
     local('git remote add origin git@github.com:%s/%s.git' % (github_username, config['$NEW_REPOSITORY_NAME']))
     local('git push -u origin master')
