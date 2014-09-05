8d28acc8fe8a26ff3ebeaa343d9fbddcd7421b69
Tyler Fisher <tyler@tylerjfisher.com>
2014-09-05 14:17:51 -0400

    if COPY_GOOGLE_DOC_URL is none, don't try to update the spreadsheet. closes #411

--- fabfile/text.py
+++ fabfile/text.py
@@ -5,6 +5,7 @@ Commands related to syncing copytext from Google Docs.
 """
 
 from fabric.api import task
+from termcolor import colored
 
 import app_config
 from etc.gdocs import GoogleDoc
@@ -14,13 +15,17 @@ def update():
     """
     Downloads a Google Doc as an Excel file.
     """
-    doc = {}
-    url = app_config.COPY_GOOGLE_DOC_URL
-    bits = url.split('key=')
-    bits = bits[1].split('&')
-    doc['key'] = bits[0]
+    if app_config.COPY_GOOGLE_DOC_URL == None:
+        print colored('You have set COPY_GOOGLE_DOC_URL to None. If you want to use a Google Sheet, set COPY_GOOGLE_DOC_URL to the URL of your sheet in app_config.py', 'blue')
+        return
+    else:
+        doc = {}
+        url = app_config.COPY_GOOGLE_DOC_URL
+        bits = url.split('key=')
+        bits = bits[1].split('&')
+        doc['key'] = bits[0]
 
-    g = GoogleDoc(**doc)
-    g.get_auth()
-    g.get_document()
+        g = GoogleDoc(**doc)
+        g.get_auth()
+        g.get_document()
 
