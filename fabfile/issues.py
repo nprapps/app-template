476fe61e103eaa580991c845897258cd87595f15
Christopher Groskopf <staringmonkey@gmail.com>
2014-12-11 10:24:33 -0600

    Don't allow issues bootstrap in app-template repo. Closes #490.

--- fabfile/issues.py
+++ fabfile/issues.py
@@ -6,6 +6,7 @@ Commands that integrate with Github issues.
 
 from fabric.api import task
 
+import app_config
 from etc import github
 
 @task
@@ -13,6 +14,10 @@ def bootstrap():
     """
     Bootstraps Github issues with default configuration.
     """
+    if app_config.PROJECT_SLUG == '$NEW_PROJECT_SLUG':
+        print 'You can\'t run the issues bootstrap until you\'ve set PROJECT_SLUG in app_config.py!'
+        return
+
     auth = github.get_auth()
     github.delete_existing_labels(auth)
     github.create_labels(auth)
