677ddd0fa6ab716281de5c0eab22f102ffb6e2ff
David Eads <davideads@gmail.com>
2015-03-02 13:09:46 -0500

    rearrange templates and fix minor issues

--- oauth.py
+++ oauth.py
@@ -3,8 +3,10 @@ import os
 
 from app_config import authomatic
 from authomatic.adapters import WerkzeugAdapter
-from flask import Blueprint, redirect, url_for
+from etc.gdocs import GoogleDoc
+from flask import Blueprint, make_response, redirect, render_template, url_for
 from functools import wraps
+from render_utils import make_context
 
 oauth = Blueprint('oauth', __name__)
 
@@ -14,14 +16,14 @@ def oauth_alert():
     Show an OAuth alert to start authentication process.
     """
     context = make_context()
-
     credentials = get_credentials()
+
     if credentials:
         resp = authomatic.access(credentials, 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json')
         if resp.status == 200:
             context['email'] = resp.data['email']
 
-    return render_template('oauth.html', **context)
+    return render_template('oauth/oauth.html', **context)
 
 @oauth.route('/authenticate/', methods=['GET', 'POST'])
 def authenticate():
@@ -31,6 +33,7 @@ def authenticate():
     from flask import request
     response = make_response()
     context = make_context()
+
     result = authomatic.login(WerkzeugAdapter(request, response), 'google')
 
     if result:
@@ -47,7 +50,7 @@ def authenticate():
             g = GoogleDoc(**doc)
             g.get_document()
 
-        return render_template('authenticate.html', **context)
+        return render_template('oauth/authenticate.html', **context)
 
     return response
 
