4ab985174a7a0b13b958b7afbf1f0350e42b1a65
Christopher Groskopf <staringmonkey@gmail.com>
2015-04-10 16:31:57 -0500

    Revert "Experimental dual-GA support. #549."
    This reverts commit 1d7f87b22357ba2ec334f128c23720b98026a0cb.

--- www/js/analytics.js
+++ www/js/analytics.js
@@ -64,23 +64,12 @@ var ANALYTICS = (function () {
 
         _gaq.push(['_trackPageview']);
 
-        // Old GA: NPR.org
         (function() {
             var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
             ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
             var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
         })();
-
-        // New GA: NPR Visuals
-        (function(i,s,o,g,r,a,m) {
-            i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-5828686-75', 'auto');
-        ga('send', 'pageview');
-     }
+    }
 
     /*
      * Comscore
@@ -158,7 +147,7 @@ var ANALYTICS = (function () {
             args.push(value);
         }
 
-        ga.send.apply(this, args);
+        _gaq.push(args);
     }
 
     // SHARING
