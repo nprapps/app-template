10b1003ee2ff754c187cd06e33ab6268f2c1f54a
David Eads <davideads@gmail.com>
2015-05-06 10:47:16 -0400

    port graeae's fab methods to install google oauth creds to server

--- fabfile/servers.py
+++ fabfile/servers.py
@@ -31,6 +31,7 @@ def setup():
 
         return
 
+    install_google_oauth_creds()
     create_directories()
     create_virtualenv()
     clone_repo()
@@ -115,6 +116,22 @@ def uninstall_crontab():
     require('settings', provided_by=['production', 'staging'])
 
     sudo('rm /etc/cron.d/%(PROJECT_FILENAME)s' % app_config.__dict__)
+
+@task
+def install_google_oauth_creds():
+    """
+    Install Google Oauth credentials file (global) from workinprivate repo
+    """
+    run('git clone git@github.com:nprapps/workinprivate.git /tmp/workinprivate-tmp')
+    run('cp /tmp/workinprivate-tmp/.google_oauth_credentials %s' % OAUTH_CREDENTIALS_PATH)
+    run('rm -Rf /tmp/workinprivate-tmp')
+
+@task
+def remove_google_oauth_creds():
+    """
+    Remove Google oauth credentials file (global)
+    """
+    run('rm %s' % OAUTH_CREDENTIALS_PATH)
 
 def delete_project():
     """
