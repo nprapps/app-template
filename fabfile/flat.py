d34ed38e403e33841f5d034fa86ec22e9bb83018
Tyler Fisher <tyler@tylerjfisher.com>
2015-05-15 16:20:50 -0400

    fix deployment. closes #556

--- fabfile/flat.py
+++ fabfile/flat.py
@@ -41,7 +41,7 @@ def deploy_file(connection, src, dst, headers={}):
     file_headers = copy.copy(headers)
 
     if 'Content-Type' not in headers:
-        file_headers['Content-Type'] = mimetypes.guess_type(src)[0],
+        file_headers['Content-Type'] = mimetypes.guess_type(src)[0]
 
     # Gzip file
     if os.path.splitext(src)[1].lower() in GZIP_FILE_TYPES:
