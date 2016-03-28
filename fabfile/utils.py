5fbccfa65b6126b5ee52434dd17ae08bb596f112
Tyler Fisher <tyler@tylerjfisher.com>
2016-03-28 15:00:07 -0400

    load fontello from fontello script

--- fabfile/utils.py
+++ fabfile/utils.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import boto
+
 from boto.s3.connection import OrdinaryCallingFormat
+from fabric.api import local, task
 
 """
 Utilities used by multiple commands.
@@ -30,3 +32,27 @@ def get_bucket(bucket_name):
         s3 = boto.connect_s3()
 
     return s3.get_bucket(bucket_name)
+
+@task
+def install_font(force='true'):
+    """
+    Install font
+    """
+    print 'Installing font'
+    if force != 'true':
+        try:
+            with open('www/css/icon/elections16.css') and open('www/css/font/elections16.svg'):
+                print 'Font installed, skipping.'
+                return
+        except IOError:
+            pass
+
+    local('node_modules/fontello-cli/bin/fontello-cli install --config fontello/config.json --css www/css/icon --font www/css/font/')
+
+
+@task
+def open_font():
+    """
+    Open font in Fontello GUI in your browser
+    """
+    local('node_modules/fontello-cli/bin/fontello-cli open --config fontello/config.json')
