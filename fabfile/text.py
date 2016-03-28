2084383f1447be28447f343076d06f466f62a333
Tyler Fisher <tyler@tylerjfisher.com>
2016-03-28 13:54:43 -0400

    colorama doesn't work with logger. that's okay

--- fabfile/text.py
+++ fabfile/text.py
@@ -9,7 +9,6 @@ import logging
 
 from fabric.api import task
 from oauth import get_document, get_credentials
-from termcolor import colored
 
 logging.basicConfig(format=app_config.LOG_FORMAT)
 logger = logging.getLogger(__name__)
@@ -21,13 +20,13 @@ def update():
     Downloads a Google Doc as an Excel file.
     """
     if app_config.COPY_GOOGLE_DOC_KEY == None:
-        logger.warn(colored('You have set COPY_GOOGLE_DOC_KEY to None. If you want to use a Google Sheet, set COPY_GOOGLE_DOC_KEY  to the key of your sheet in app_config.py', 'blue'))
+        logger.warn('You have set COPY_GOOGLE_DOC_KEY to None. If you want to use a Google Sheet, set COPY_GOOGLE_DOC_KEY  to the key of your sheet in app_config.py')
         return
 
     credentials = get_credentials()
     if not credentials:
-        print logger.warn(colored('No Google OAuth credentials file found.', 'yellow'))
-        print logger.warn(colored('Run `fab app` and visit `http://localhost:8000` to generate credentials.', 'yellow'))
+        print logger.warn('No Google OAuth credentials file found.')
+        print logger.warn('Run `fab app` and visit `http://localhost:8000` to generate credentials.')
         return
 
     get_document(app_config.COPY_GOOGLE_DOC_KEY, app_config.COPY_PATH)
