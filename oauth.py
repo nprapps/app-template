d21517e9f746eba2e6576a0e6ff9fa8e55d7011f
jjelosua <jelosua@npr.org>
2017-04-12 13:23:17 -0400

    change download url to google drive v3 api

--- oauth.py
+++ oauth.py
@@ -8,7 +8,9 @@ from flask import Blueprint, make_response, redirect, render_template, url_for
 from functools import wraps
 from render_utils import make_context
 
-SPREADSHEET_URL_TEMPLATE = 'https://docs.google.com/feeds/download/spreadsheets/Export?exportFormat=xlsx&key=%s'
+# Via: https://developers.google.com/drive/v3/reference/files/export
+# and: https://developers.google.com/drive/v3/web/manage-downloads
+DRIVE_API_EXPORT_TEMPLATE = 'https://www.googleapis.com/drive/v3/files/%s/export?mimeType=%s'
 
 oauth = Blueprint('_oauth', __name__)
 
@@ -99,12 +101,18 @@ def save_credentials(credentials):
     with open(file_path, 'w') as f:
         f.write(credentials.serialize())
 
-def get_document(key, file_path):
+def get_document(key, file_path, mimeType=None):
     """
     Uses Authomatic to get the google doc
     """
+    # Default to spreadsheet if no mimeType is passed
+    mime = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    if not mimeType:
+        mimeType = mime
     credentials = get_credentials()
-    url = SPREADSHEET_URL_TEMPLATE % key
+    url = DRIVE_API_EXPORT_TEMPLATE % (
+        key,
+        mimeType)
     response = app_config.authomatic.access(credentials, url)
 
     if response.status != 200:
